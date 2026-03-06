686d97f403805d049ae45b99bc47b0f9eee608ed
Tyler <ty@zzox.net>
2026-03-06 15:13:14 -0600

    top items

--- src/game/scenes/UiScene.hx
+++ src/game/scenes/UiScene.hx
@@ -1,10 +1,12 @@
 package game.scenes;
 
 import core.Game;
+import core.gameobjects.BitmapText;
 import core.scene.Scene;
 import game.ui.GeneSelectWindow;
 import game.ui.UiElement;
 import game.ui.UiText;
+import game.util.TextUtil;
 import game.world.Run;
 import kha.Assets;
 import kha.graphics2.Graphics;
@@ -12,9 +14,13 @@ import kha.input.Mouse;
 
 class UiScene extends ButtonScene {
     var windows:Array<GeneSelectWindow> = [];
+    var moneyText:BitmapText;
 
     override function update (delta:Float) {
         super.update(delta);
+
+        moneyText.setText(TextUtil.formatMoney(Run.inst.money));
+        moneyText.x = 320 - moneyText.textWidth - 4;
 
         for (win in windows) {
             for (c in win.children) {
@@ -50,21 +56,15 @@ class UiScene extends ButtonScene {
         g2.end();
     }
 
-    // TODO: following to parent scene
     function makeTopButtons (sceneIndex:Int) {
-        // final button = makeUiButton(64, 0, 32, 16, 16, () -> { trace('click'); });
-        // final icon = new Sprite(DayTimeWidth + pos * 32, 0, Assets.images.ui, 32, 32);
-        // icon.tileIndex = imgIndex + 48;
+        entities.push(new UiElement(0, 0, 16, 16, 4, 4, 12, 12, 64, 16, 25, Assets.images.ui));
+        entities.push(new UiElement(320 - 64, 0, 16, 16, 4, 4, 12, 12, 64, 16, 25, Assets.images.ui));
 
-        buttons.push(makeUiTextButton(100, 0, 40, 16, 16, 'BTTL', sceneIndex == 0 ? () -> {
-            trace('play sound!');
-        } : () -> {
+        buttons.push(makeUiTextButton(100, 0, 40, 16, sceneIndex == 0 ? 21 : 16, 'BTTL', sceneIndex == 0 ? null : () -> {
             game.changeScene(new PreBattleScene());
         }));
 
-        buttons.push(makeUiTextButton(140, 0, 40, 16, 16, 'SYNC', sceneIndex == 1 ? () -> {
-            trace('play sound!');
-        } : () -> {
+        buttons.push(makeUiTextButton(140, 0, 40, 16, sceneIndex == 1 ? 21 : 16, 'SYNC', sceneIndex == 1 ? null : () -> {
             game.changeScene(new SyncScene());
         }));
 
@@ -72,7 +72,10 @@ class UiScene extends ButtonScene {
             trace('clicked!');
         }));
 
-        // entities.push(icon);
-        // entities.push(button);
+        moneyText = makeBitmapText(0, 0, TextUtil.formatMoney(Run.inst.money), 0xfffc40);
+        moneyText.x = 320 - moneyText.textWidth - 4;
+
+        entities.push(makeBitmapText(4, 0, 'Day ${Run.inst.day}', 0xdae0ea));
+        entities.push(moneyText);
     }
 }
