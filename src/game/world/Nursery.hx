423b926da319ec3885a9548287eb81048566adf3
Tyler <ty@zzox.net>
2026-03-10 10:05:41 -0500

    radiation fix, v0.0.1

--- src/game/world/Nursery.hx
+++ src/game/world/Nursery.hx
@@ -10,6 +10,6 @@ class Nursery {
         final t1 = Lambda.fold(dna1.genes, (item:Gene, res:Int) -> res + item, 0);
         final t2 = Lambda.fold(dna2.genes, (item:Gene, res:Int) -> res + item, 0);
         parents = [dna1, dna2];
-        children = combineDna(dna1, dna2, (dna1.rad + dna2.rad) / 100, (t1 + t2) % 4 + 1);
+        children = combineDna(dna1, dna2, (dna1.rad + dna2.rad), (t1 + t2) % 4 + 1);
     }
 }
